cc635d5170f4b2d18331aa8c9b149dc33c3a7b02
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-07 10:59:04 +0900

    Sync random generation phases 2-9

--- src/world/VoxelDefinitions.ts
+++ src/world/VoxelDefinitions.ts
@@ -6,6 +6,8 @@ export enum VoxelType {
   Rock = 2,
   Ore = 3,
   Bedrock = 4,
+  Wood = 5,
+  Leaves = 6,
 }
 
 export interface VoxelDefinition {
@@ -20,4 +22,6 @@ export const VOXEL_DEFINITIONS: Record<VoxelType, VoxelDefinition> = {
   [VoxelType.Rock]: { maxHealth: 2, destructible: true, color: new THREE.Color(0x657083) },
   [VoxelType.Ore]: { maxHealth: 3, destructible: true, color: new THREE.Color(0x23b6b8) },
   [VoxelType.Bedrock]: { maxHealth: 255, destructible: false, color: new THREE.Color(0x20283a) },
+  [VoxelType.Wood]: { maxHealth: 2, destructible: true, color: new THREE.Color(0x9a613d) },
+  [VoxelType.Leaves]: { maxHealth: 1, destructible: true, color: new THREE.Color(0x4d9b68) },
 };
